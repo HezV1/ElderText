abde8d27421d5a199b808abbf0aff2b7f6c34796
Conrad Hoffman <conrad.r.hoffman@gmail.com>
2015-03-10 13:54:28 -0500

    worked on game

--- embershard_mine.py
+++ embershard_mine.py
@@ -1,15 +1,27 @@
 from elder_level import Level
+from sys import exit
 
 class StartPath(object):
     """class for StartPath Scene"""
     def enter(self):
-        print "While walking along the road to Riverwood, you notice an area"
-        print "Between two of the trees you can see a path worn away in the"
-        print "dirt. Upon the slope of the path that leads north, you can see"
-        print "a few wooden slats embeded in the ground. The path curves to"
-        print "the right behind some shrubs."
-        OPTIONS =['go']
-        print OPTIONS
+        print """
+You are walking north-east along the path to Riverwood. The white river
+gurgles and bubbles as it rolls past you. As you check your surroundings, you
+notice a path worn into the grass on your right. The path veers of the road
+to the east and and curves around the stone outcropping to the south-east.
+Inlaid into the incline of the path are wooden slats. A tree borders the path
+on the left and sparse shrubbery can be found on the inside of the curve of
+the path.
+"""
+        while True:
+            OPTIONS =['follow path']
+            print OPTIONS
+            choice = raw_input()
+            if choice == OPTIONS[0]:
+                break
+            else:
+                pass
+
         return "entrance"
 
 class Entrance(object):
