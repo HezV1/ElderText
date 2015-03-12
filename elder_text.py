bdf7f464792c45c296066fe619b751e1a5246f04
unknown <15choffman@HHS011687.internal.usd489.com>
2015-03-12 18:16:43 -0500

    added level selection

--- elder_text.py
+++ elder_text.py
@@ -1,14 +1,14 @@
 from sys import exit, argv
+import importlib
 print ("*** loading engine ***")
 from elder_engine import Engine
 print ("*** loading level ***")
-
-
-SubLevel
+level = raw_input('Grab Level Code')
+level_b = importlib.import_module(level, 'SubLevel')
 
 OPTIONS = []
 
-level_a = SubLevel()
+level_a = level_b.SubLevel()
 engine_a = Engine(level_a)
 engine_a.play()
 
